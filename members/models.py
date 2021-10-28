fef4acdef620ac9ceecdcceb4247f89c57ee668c
chaiwon <c.park-3@student.tudelft.nl>
2021-10-28 05:55:18 +0200

    'last_changed' added to passwords table

--- members/models.py
+++ members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .forms import SetPasswordForm
 
 # Create your models here.
 
@@ -9,7 +10,23 @@ class Members(models.Model):
     created_at = models.DateField(null=True)
     active = models.BooleanField(null=True)
 
+    def __str__(self):
+        return self.username
+
 class Passwords(models.Model):
     member_id = models.ForeignKey(Members, on_delete=models.CASCADE)
     username = models.CharField(max_length=70, unique=True)
     password = models.CharField(max_length=120)
+    last_changed = models.DateField(null=True)
+
+    def __str__(self):
+        return self.username
+
+    def save(self, commit=True):
+        password = super(SetPasswordForm, self).save(commit=False)
+        username, password = self.cleaned_data["username"].split()
+        password.username = username
+        password.password = password
+        if commit:
+            password.save()
+        return password
