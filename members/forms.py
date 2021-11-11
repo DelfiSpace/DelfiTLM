28c35759fd427df13d24c6deee931f054d7de713
chaiwon <c.park-3@student.tudelft.nl>
2021-11-11 10:31:55 +0100

    Setting up the password works

---
+++ members/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Passwords
+
+class SetPasswordForm(forms.ModelForm):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(label="password")
+    confirm_password = forms.CharField(label="confirm password")
+
+    class Meta:
+        model = Passwords
+        fields = ["username", "password"]
--- members/models.py
+++ members/models.py
@@ -1,32 +1,30 @@
+import uuid
 from django.db import models
-from .forms import SetPasswordForm
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class Members(models.Model):
-    username = models.CharField(max_length=70, unique=True)
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=70)
     email = models.CharField(max_length=320, unique=True)
-    role = models.CharField(max_length=60)
-    created_at = models.DateField(null=True)
-    active = models.BooleanField(null=True)
+    role = models.CharField(max_length=60, null=True, blank=True)
+    created_at = models.DateField(null=True, blank=True)
+    active = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
         return self.username
 
 class Passwords(models.Model):
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     member_id = models.ForeignKey(Members, on_delete=models.CASCADE)
-    username = models.CharField(max_length=70, unique=True)
-    password = models.CharField(max_length=120)
-    last_changed = models.DateField(null=True)
+    username = models.CharField(max_length=70)
+    password = models.CharField(max_length=120,
+                                validators=[
+                                    RegexValidator(regex='^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+                                                   message='Enter password containing min 8 characters, min 1 upper and lower case letter, 1 number and 1 special character')
+                                ])
+    last_changed = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.username
-
-    def save(self, commit=True):
-        password = super(SetPasswordForm, self).save(commit=False)
-        username, password = self.cleaned_data["username"].split()
-        password.username = username
-        password.password = password
-        if commit:
-            password.save()
-        return password
