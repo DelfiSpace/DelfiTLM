1339edc2bb0777dd30d9627ce87dae6a1ccd20ac
nicusoara1 <nicomariagheorghe@yahoo.com>
2021-11-14 14:48:03 +0100

    Issue #12
    Created the telemetry table for delfi n3xt

--- src/delfin3xt/models.py
+++ src/delfin3xt/models.py
@@ -1,3 +1,15 @@
-# from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+
+class Delfin3xt_L0_telemetry(models.Model):
+    counter = models.PositiveIntegerField(primary_key=True, null=False)
+    frame_time = models.IntegerField(default=None, null=True)
+    send_time = models.IntegerField(default=None, null=True)
+    receive_time = models.TimeField(auto_now_add=True, null=False, default = datetime.time )
+    radio_amateur = models.IntegerField(default=None, null=True)
+    frame = models.BinaryField(default=None, null=True)
+    version = models.TextField
+    processed = models.BooleanField(default=False, null=False)
+    frequency = models.FloatField(default=None, null=True)
+    qos = models.FloatField(default=None, null=True)
