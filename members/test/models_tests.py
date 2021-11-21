c818d8c7df16c9d0f13fb4638353e5a38d155f07
chaiwon <c.park-3@student.tudelft.nl>
2021-11-21 16:00:59 +0100

    Tests added.

--- members/test/models_tests.py
+++ members/test/models_tests.py
@@ -1,11 +1,20 @@
-# from django.test import TestCase
-# from members.models import Members
-#
-# class MembersTestCase(TestCase):
-#     def setUp(self):
-#         Members.objects.create(name="userA", email="user@email.com", role="userRole", created_at="10/22/21", active=True)
-#         Members.objects.create(name="userB", email="user@email.com", role="userRole", created_at="10/22/21", active=True)
-#
-#     def test_members(self):
-#         userA = Members.objects.get(name="userA")
-#         self.assertEqual(userA.email, "user@email.com")
+from django.test import TestCase
+from ..models import Member
+
+class MemberModelTestCase(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.member = Member.objects.create(
+            name="userA",
+            email="user@email.com",
+            password="testpassword",
+            role="userRole",
+            created_at="10/22/21",
+            active=True
+        )
+        # Members.objects.create(name="userA", email="user@email.com", role="userRole", created_at="10/22/21", active=True)
+
+    def test_members(self):
+        userA = Member.objects.get(name="userA")
+        self.assertEqual(userA.email, "user@email.com")
--- members/test/test_views_templates.py
+++ members/test/test_views_templates.py
@@ -1,7 +1,10 @@
 """Test views html templates"""
 from django.test import SimpleTestCase, Client, TestCase
 from django.urls import reverse
-
+from django.test.client import Client
+from ..models import Member
+# import unittest
+# from django.contrib.auth.models import User
 
 # pylint: disable=all
 
@@ -14,11 +17,40 @@ class TestViews(SimpleTestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'ewilgs/home/index.html')
 
-class TestCalls(TestCase):
-    def test_call_home_page(self):
+class LoginTestCase(TestCase):
+    def setUp(self):
         self.client = Client()
-        self.list_url = reverse('members_home')
-        response = self.client.get(self.list_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'members/home/index.html')
+        self.user = Member.objects.create_user("test", "test@email,com", "testpassword")
+
+    def testLogin(self):
+        self.client.login(user='test', password='testpassword')
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+class HomeTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Member.objects.create_user("test", "test@email,com", "testpassword")
+
+    def userLoggedIn(self):
+        self.client.login(user='test', password='testpassword')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 302)
+
+    def userNotLoggedIn(self):
+        # self.client.login(user='test', password='testpassword')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 400)
+
+class SetPasswordTestCase(TestCase):
+    def SetTestCase(self):
+        # self.client.login(user='test', password='testpassword')
+        response = self.client.get(reverse('set'))
+        self.assertEqual(response.status_code, 200)
+
+class ChangePasswordTestCase(TestCase):
+    def ChangeTestCase(self):
+        # self.client.login(user='test', password='testpassword')
+        response = self.client.get(reverse('change'))
+        self.assertEqual(response.status_code, 200)
 
