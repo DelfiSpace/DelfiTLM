845337c2854e4312bfb372d8b6bc736c7704b632
nicusoara1 <nicomariagheorghe@yahoo.com>
2021-11-21 12:06:28 +0100

    Issue #12
    Created the telemetry table for delfi n3xt

--- src/delfin3xt/admin.py
+++ src/delfin3xt/admin.py
@@ -3,7 +3,4 @@
 from django.contrib import admin
 from .models import Delfin3xt_L0_telemetry
 
-@admin.register(Delfin3xt_L0_telemetry)
-class Delfin3xt_L0_telemetry_Admin(admin.ModelAdmin): #pylint: disable=C0103
-    """Registering to the delfin3xt telemetry to the admin interface"""
-    pass #pylint: disable=W0107
+admin.site.register(Delfin3xt_L0_telemetry)
