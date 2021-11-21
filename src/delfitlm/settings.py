409d8ade7c3d0aafc8227b779be2e271e98240f3
NotUnlikeTheWaves <hleistra@gmail.com>
2021-11-21 10:25:05 +0100

    Set up postgres db, set right code watch path

--- src/delfitlm/settings.py
+++ src/delfitlm/settings.py
@@ -94,10 +94,15 @@ WSGI_APPLICATION = 'delfitlm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
