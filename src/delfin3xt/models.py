c92c0fb1f980e49f23e8926b5ef607851c7116ee
nicusoara1 <nicomariagheorghe@yahoo.com>
2021-11-28 13:13:22 +0100

    Created the telemetry table for delfi n3xt

--- src/delfin3xt/models.py
+++ src/delfin3xt/models.py
@@ -8,10 +8,10 @@ class Delfin3xt_L0_telemetry(models.Model): #pylint: disable=C0103
     """
     Telemetry Table Delfin3xt
     """
-    counter = models.PositiveIntegerField(primary_key=True, null=False)
-    frame_time = models.IntegerField(default=None, null=True)
-    send_time = models.IntegerField(default=None, null=True)
-    receive_time = models.TimeField(null=False, default = datetime.time )
+    id = models.PositiveIntegerField(primary_key=True, null=False)
+    frame_time = models.TimeField(null=False, default=datetime.time, auto_now=False, auto_now_add=False) #pylint: disable=C0301
+    send_time = models.TimeField(null=False, default=datetime.time, auto_now=False, auto_now_add=False) #pylint: disable=C0301
+    receive_time = models.TimeField(null=False, default=datetime.time, auto_now=False, auto_now_add=False) #pylint: disable=C0301
     radio_amateur = models.IntegerField(default=None, null=True)
     frame = models.BinaryField(default=None, null=True)
     version = models.TextField
