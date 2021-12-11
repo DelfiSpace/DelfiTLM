420a88d09db4512475a4e741676cde44c59c374a
NotUnlikeTheWaves <hleistra@gmail.com>
2021-12-11 23:52:22 +0100

    Try fix linting error

--- src/members/backend/authentication_backend.py
+++ src/members/backend/authentication_backend.py
@@ -7,7 +7,7 @@ from ..models import Member
 class AuthenticationBackend(BaseBackend):
     """Custom authentication backend"""
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, request, username=None, password=None):
         """Authentication method"""
 
         if Member.objects.filter(username=username).exists():
