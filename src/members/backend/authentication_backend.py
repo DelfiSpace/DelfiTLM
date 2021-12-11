e5bbbfa7d70a60ca376d70c573076379e33f67e3
NotUnlikeTheWaves <hleistra@gmail.com>
2021-12-12 00:12:29 +0100

    Fix linter error

--- src/members/backend/authentication_backend.py
+++ src/members/backend/authentication_backend.py
@@ -7,7 +7,7 @@ from ..models import Member
 class AuthenticationBackend(ModelBackend):
     """Custom authentication backend"""
 
-    def authenticate(self, request, username=None, password=None):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         """Authentication method"""
 
         if Member.objects.filter(username=username).exists():
