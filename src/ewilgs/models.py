82b146ba1172d8237253085832446404a25d23cc
nicusoara1 <nicomariagheorghe@yahoo.com>
2021-12-12 13:59:40 +0100

    merged 24 with main to fix pylint

--- src/ewilgs/models.py
+++ src/ewilgs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from members.models import Members
+from members.models import Member
 
 class Downlink(models.Model):
     received_at = models.TimeField(auto_now_add=True, null=False)
@@ -8,7 +8,7 @@ class Downlink(models.Model):
     processed = models.BooleanField(null=True) #NULL values are allowed for this field
 
 class Uplink(models.Model):
-    UUID_user = models.ForeignKey(Members, editable=False,on_delete=models.CASCADE)
+    UUID_user = models.ForeignKey(Member, editable=False,on_delete=models.CASCADE)
     created_at = models.TimeField(auto_now=False, auto_now_add=False, null=False)
     transmitted_at = models.TimeField(auto_now=False, auto_now_add=False, null=True) #auto_now: last edited timestamp is registered
     data = models.BooleanField(null=True)
