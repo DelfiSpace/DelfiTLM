74b623358856d4270ea75d9d46cf2071289eecf9
Paula Iacoban <i.p.iacoban@gmail.com>
2022-01-07 02:53:27 +0100

    Add tests for the login

--- src/members/test/test_views_templates.py
+++ src/members/test/test_views_templates.py
@@ -17,39 +17,251 @@ class TestViews(SimpleTestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'ewilgs/home/index.html')
 
-class LoginTestCase(TestCase):
+class TestLogin(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = Member.objects.create_user("test", "test@email,com", "testpassword")
+        self.user = Member.objects.create_user(username='user', email='user@email.com')
+        self.user.set_password('delfispace4242')
+        self.user.save()
 
-    def testLogin(self):
-        self.client.login(user='test', password='testpassword')
+    def teardown(self):
+        self.client.logout()
+
+    def test_login(self):
+        # login form retrieved
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        # login request successful
+        response = self.client.post(reverse('login'), {'username': 'user', 'password': 'delfispace4242'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        # login form retrieved
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
-class HomeTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = Member.objects.create_user("test", "test@email,com", "testpassword")
+        # login request successful
+        response = self.client.post(reverse('login'), {'username': 'user', 'password': 'delfispace4242'})
+        self.assertEqual(response.status_code, 200)
 
-    def userLoggedIn(self):
-        self.client.login(user='test', password='testpassword')
-        response = self.client.get(reverse('home'))
+        # logout request successful, redirect to homepage
+        response = self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 302)
 
-    def userNotLoggedIn(self):
-        # self.client.login(user='test', password='testpassword')
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 400)
 
-class SetPasswordTestCase(TestCase):
-    def SetTestCase(self):
-        # self.client.login(user='test', password='testpassword')
-        response = self.client.get(reverse('set'))
+    def test_login_wrong_credentials(self):
+        # login form retrieved
+        response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
-class ChangePasswordTestCase(TestCase):
-    def ChangeTestCase(self):
-        # self.client.login(user='test', password='testpassword')
-        response = self.client.get(reverse('change'))
+        # empty username field
+        response = self.client.post(reverse('login'), {'username': '', 'password': 'delfispace4242'})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Invalid username or password')
+
+        # empty password field
+        response = self.client.post(reverse('login'), {'username': 'user', 'password': ''})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Invalid username or password')
+
+        # wrong username
+        response = self.client.post(reverse('login'), {'username': 'admin', 'password': 'delfispace4242'})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Invalid username or password')
+
+        # wrong password
+        response = self.client.post(reverse('login'), {'username': 'user', 'password': 'delfispace424'})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Invalid username or password')
+
+        # wrong username and password
+        response = self.client.post(reverse('login'), {'username': 'user1', 'password': 'delfispace424'})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Invalid username or password')
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Member.objects.create_user(username='user', email='test@email.com')
+        self.user.set_password('delfispace4242')
+        self.user.save()
+
+    def teardown(self):
+        self.client.logout()
+
+    def test_user_logged_in(self):
+        # the user is logged in and can view the profile page
+        login = self.client.login(username='user', password='delfispace4242')
+        self.assertTrue(login)
+        response = self.client.get(reverse('profile'))
         self.assertEqual(response.status_code, 200)
+
+    def test_user_not_logged_in(self):
+        # the user is not logged in and since profile is login protected the user is redirected to login
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 302)
+
+class TestRegister(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = Member.objects.create_user(username='user', email='user@email.com')
+        self.user.set_password('delfispace4242')
+        self.user.save()
+
+    def teardown(self):
+        self.client.logout()
+
+    def test_register(self):
+        # register form successfully retrieved
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_post_request(self):
+        # register form successfully retrieved
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse('register'), {'username': 'user2',
+                                                          'email': 'test2@email.com',
+                                                          'password1': 'delfispace4242',
+                                                          'password2': 'delfispace4242'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_user_already_exists(self):
+        # register form successfully retrieved
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse('register'), {'username': 'user',
+                                                          'email': 'test@email.com',
+                                                          'password1': 'delfispace4242',
+                                                          'password2': 'delfispace4242'})
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), 'Unsuccessful registration. Invalid information.')
+
+    def test_register_invalid_email(self):
+        # register form successfully retrieved
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        # bad email
+        response = self.client.post(reverse('register'), {'username': 'user3',
+                                                          'email': 'test',
+                                                          'password1': 'delfispace4242',
+                                                          'password2': 'delfispace4242'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+    def test_register_invalid_password(self):
+        # register form successfully retrieved
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+        # password only numeric
+        response = self.client.post(reverse('register'), {'username': 'user3',
+                                                          'email': 'test3@email.com',
+                                                          'password1': '1155239711568432464',
+                                                          'password2': '1155239711568432464'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+        # password too short
+        response = self.client.post(reverse('register'), {'username': 'user3',
+                                                          'email': 'test3@email.com',
+                                                          'password1': 'short',
+                                                          'password2': 'short'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+        # password same as username
+        response = self.client.post(reverse('register'), {'username': 'qwertyuser',
+                                                          'email': 'test3@email.com',
+                                                          'password1': 'qwertyuser',
+                                                          'password2': 'qwertyuser'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+        # password missmatch
+        response = self.client.post(reverse('register'), {'username': 'user3',
+                                                          'email': 'test3@email.com',
+                                                          'password1': 'delfispace4242',
+                                                          'password2': 'delfispace424'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+
+class TestChangePassword(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Member.objects.create_user(username='user', email='test@email.com')
+        self.user.set_password('delfispace4242')
+        self.user.save()
+
+    def teardown(self):
+        self.client.logout()
+
+    def test_change_password_user_not_logged_in(self):
+        # user is not logged in and password reset is login protected
+        # the user is redirected to the home page
+        response = self.client.get(reverse('change_password'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_change_password_user_logged_in(self):
+        # user is logged in and password reset is login protected
+        # the user receives the change password form
+        self.client.login(username='user', password='delfispace4242')
+        response = self.client.get(reverse('change_password'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_password_changed(self):
+        # user is logged in and password reset is login protected
+        # the user receives the change password form
+        self.client.login(username='user', password='delfispace4242')
+        response = self.client.get(reverse('change_password'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse('change_password'), {
+                                                          'old_password': 'delfispace4242',
+                                                          'new_password1': 'delfispace424',
+                                                          'new_password2': 'delfispace424'})
+        # redirected to the profile page
+        self.assertEqual(response.status_code, 302)
+
+    def test_change_invalid_password(self):
+        # reset password form successfully retrieved
+        self.client.login(username='user', password='delfispace4242')
+        response = self.client.get(reverse('change_password'))
+        self.assertEqual(response.status_code, 200)
+
+        # password only numeric
+        response = self.client.post(reverse('change_password'), {
+                                                          'old_password': 'delfispace4242',
+                                                          'new_password1': '1155239711568432464',
+                                                          'new_password2': '1155239711568432464'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+        # password too short
+        response = self.client.post(reverse('change_password'), {
+                                                          'old_password': 'delfispace4242',
+                                                          'new_password1': 'short',
+                                                          'new_password2': 'short'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
+
+        # new password missmatch
+        response = self.client.post(reverse('change_password'), {
+                                                           'old_password': 'delfispace4242',
+                                                          'new_password1': 'delfispace443',
+                                                          'new_password2': 'delfispace4343'})
+
+        messages = list(response.context['messages'])
+        self.assertTrue(len(messages)>0)
