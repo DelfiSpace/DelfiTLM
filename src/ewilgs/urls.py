d0fc4ea6df4624595f4f6d474e7af0c4078591b0
Paula Iacoban <i.p.iacoban@gmail.com>
2022-01-10 16:16:32 +0100

    Small fixes

--- src/ewilgs/urls.py
+++ src/ewilgs/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('ewilgs/home/', views.home, name='home'),
     path('ewilgs/downlink/', views.get_downlink_table, name='ewilgs_downlink'),
-    path('ewilgs/uplink/', views.get_uplink_table, name='ewilgs_uplink'),
+    # path('ewilgs/uplink/', views.get_uplink_table, name='ewilgs_uplink'),
     # add dummy data COMMENT THIS IN PRODUCTION
     # path('add/downlink/', views.add_dummy_downlink_frames, name='add_dummy_downlink_frames'),
     path('submit/', views.submit, name='submit'),
