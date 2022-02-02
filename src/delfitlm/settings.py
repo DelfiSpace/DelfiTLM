26128a0dc2b1224f2103b124137087efdf8d9169
Stefano Speretta <s.speretta@tudelft.nl>
2022-02-02 15:10:04 +0000

    Fixed missing / causing the build to fail

--- src/delfitlm/settings.py
+++ src/delfitlm/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
