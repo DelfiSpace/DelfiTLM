fd51ea1879a459ac63f6dc4fdd0a2f7782f87999
Stefano Speretta <s.speretta@tudelft.nl>
2022-02-03 22:11:29 +0000

    Disable CSRF cookie for REST API

--- src/ewilgs/urls.py
+++ src/ewilgs/urls.py
@@ -1,5 +1,6 @@
 """API urls"""
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -9,5 +10,5 @@ urlpatterns = [
     path('ewilgs/uplink/', views.get_uplink_table, name='ewilgs_uplink'),
     # add dummy data
     # path('add/', views.add_dummy_downlink_frames, name='add_dummy_downlink_frames'),
-    path('submit_frame/', views.submit_frame, name='submit_frame'),
+    path('submit_frame/', csrf_exempt(views.submit_frame), name='submit_frame'),
 ]
