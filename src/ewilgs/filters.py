a13b04d6d0c983ee74f550fa3687c942ebd6c8cf
Paula Iacoban <i.p.iacoban@gmail.com>
2022-02-06 15:06:25 +0100

    Add datepicker widget

--- src/ewilgs/filters.py
+++ src/ewilgs/filters.py
@@ -1,10 +1,17 @@
 """Telemetry filters"""
 import django_filters
+from django import forms
 from .models import Downlink, Uplink, TLE
 
 class TelemetryDownlinkFilter(django_filters.FilterSet):
     """Filter downlink telemetry by frame date interval, frequency and qos ranges"""
-    frame_date = django_filters.DateFromToRangeFilter(field_name="frame_time")
+    frame_date = django_filters.DateFromToRangeFilter(field_name="frame_time",
+        widget=forms.DateInput(
+            attrs={
+                'id': 'datepicker',
+                'type': 'text'
+            }
+        ))
     frequency = django_filters.RangeFilter(field_name="frequency")
     qos = django_filters.RangeFilter(field_name="qos")
     class Meta:
@@ -15,7 +22,13 @@ class TelemetryDownlinkFilter(django_filters.FilterSet):
 
 class TelemetryUplinkFilter(django_filters.FilterSet):
     """Filter uplink telemetry by frame date interval, frequency and qos ranges"""
-    frame_date = django_filters.DateFromToRangeFilter(field_name="frame_time")
+    frame_date = django_filters.DateFromToRangeFilter(field_name="frame_time",
+        widget=forms.DateInput(
+            attrs={
+                'id': 'datepicker',
+                'type': 'text'
+            }
+        ))
     frequency = django_filters.RangeFilter(field_name="frequency")
     qos = django_filters.RangeFilter(field_name="qos")
     class Meta:
@@ -26,7 +39,14 @@ class TelemetryUplinkFilter(django_filters.FilterSet):
 
 class TLEFilter(django_filters.FilterSet):
     """Filter TLEs by date and satellite"""
-    valid_from = django_filters.DateFromToRangeFilter(field_name="valid_from")
+    valid_from = django_filters.DateFromToRangeFilter(field_name="valid_from",
+        widget=forms.DateInput(
+            attrs={
+                'id': 'datepicker',
+                'type': 'text'
+            }
+        ))
+
     class Meta:
         """Meta class to specify model"""
         model = TLE
