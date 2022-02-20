3a6106297d32bc1f5fe015f352c874556789fab0
Stefano Speretta <s.speretta@tudelft.nl>
2022-02-20 13:14:21 +0100

    Rename the submission API path

--- src/ewilgs/urls.py
+++ src/ewilgs/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('ewilgs/uplink/', views.get_uplink_table, name='ewilgs_uplink'),
     # add dummy data
     # path('add/', views.add_dummy_downlink_frames, name='add_dummy_downlink_frames'),
-    path('submit_frame/', csrf_exempt(views.submit_frame), name='submit_frame'),
+    path('submit/', csrf_exempt(views.submit_frame), name='submit_frame'),
 ]
