2a4d8869853860baf5f1a8af7010bcb06be34548
iiacoban42 <i.p.iacoban@gmail.com>
2022-02-20 12:40:04 +0100

    Merge pull request #73 from iiacoban42/72-Hardcoded-hostname
    Fix hardcoded hostname

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -4,19 +4,21 @@
 CERTIFICATE=/certs/server.pem
 KEY=/certs/server.key
 
+# this is used to resolve the $host strings in the nginx conf file
+export DOLLAR='$'
+    
 # check if the certificates exist
 if [ -f "$CERTIFICATE" ] && [ -f "$KEY" ];
 then
     echo "Certificates found... using HTTPS configuration"
 
-    # this is used to resolve the $host strings in the nginx conf file
-    export DOLLAR='$'
-
     # replace environment variables in the nginx configuration file
-    envsubst < /default.conf.template > /etc/nginx/conf.d/default.conf
+    envsubst < /https.conf.template > /etc/nginx/conf.d/default.conf
 else
     echo "Certificates not found... using HTTP configuration"
-    cp /http.conf /etc/nginx/conf.d/default.conf
+    
+    # replace environment variables in the nginx configuration file
+    envsubst < /http.conf.template > /etc/nginx/conf.d/default.conf
 fi
 
 # start nginx
