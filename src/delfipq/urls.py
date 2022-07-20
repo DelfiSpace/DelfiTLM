366d21f7be10d53ec1c40f7e012f87cd817a9de4
Paula Iacoban <i.p.iacoban@gmail.com>
2022-07-20 23:25:00 +0200

    Add grafana.ini config and nginx settings

--- src/delfipq/urls.py
+++ src/delfipq/urls.py
@@ -1,8 +1,9 @@
 """API urls"""
-from django.urls import path
+from django.urls import path, URLPattern
 
 from . import views
 
 urlpatterns = [
     path('delfipq/home/', views.home, name='delfipq_home'),
+    path('grafana/<path>/', views.GraphanaProxyView.as_view(),  name='graphana-delfi-pq'),
 ]
--- src/delfipq/views.py
+++ src/delfipq/views.py
@@ -1,5 +1,14 @@
 """API request handling. Map requests to the corresponding HTMLs."""
 from django.shortcuts import render
+from revproxy.views import ProxyView
+
+class GraphanaProxyView(ProxyView):
+    upstream = 'http://localhost:3000/grafana'
+
+    def get_proxy_request_headers(self, request):
+       headers = super(GraphanaProxyView, self).get_proxy_request_headers(request)
+       headers['X-WEBAUTH-USER'] = "Bearer eyJrIjoiQlBXUzNEWkFKWnJZS2NZVWExM3pxSWllbTBPUmx3aGwiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
+       return headers
 
 def home(request):
     """render index.html page"""
